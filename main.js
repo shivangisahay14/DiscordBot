483c171d8c04f77b85290f690a5d362f2207789c
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2021-01-28 13:16:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+
+
+client.once('ready', () =>{
+    console.log('thegoofymonkey is online!');
+});
+
+
+
+client.login('Nzk3MDQ0ODA0NzcyMDM2NjA4.X_gv0Q.4cTJlMgnsw1Ll_UVNBEuiwQqdXw');
